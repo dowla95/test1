3758e64f9ca0a279e19a3499d18dc4061fa04c6c
dowla95 <32300065+dowla95@users.noreply.github.com>
2017-09-26 16:40:33 +0200

    test2

---
+++ Script.js
@@ -0,0 +1,61 @@
+var http = require('https');
+var querystring = require ('querystring');
+
+export Script = function (req, res) {
+	res.render( 'Script', { title: 'Express'});
+};
+
+exports.index = function (req, res) {
+	var paymentToken = generatedCheckoutId { function ( result ) {
+	if ( result != null && result !=undefined )
+		res.render('index', {checkoutId: result.id });
+	else 
+		res.render('index', 'none')
+	};
+};
+
+
+function generatedCheckoutId(callback) {
+	var path='/v1/checkouts';
+	var data = querystring.stringify( {
+		'authentication.userId' : '8a8294184f45ce7e014f4b1d16cc12df',
+		'authentication.password' : 'j3zCJ2ENaD',
+		'authentication.entityId' : '8a8294184f45ce7e014f4b1d16bd12db',
+		'amount' : '92.00',
+		'currency' : 'RSD',
+		'paymentType' : 'DB'
+	});
+	var options = {
+		port: 443,
+		host: 'test.oppwa.com',
+		path: path,
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'Content-Length': data.length
+		}
+	};
+	var postRequest = http.request(options, function(res) {
+		res.setEncoding('utf8');
+		res.on('data', function (chunk) {
+			jsonRes = JSON.parse(chunk);
+			return callback(jsonRes);
+		});
+	});
+	postRequest.write(data);
+	postRequest.end();
+}
+
+exports.result = function (req, res) {
+	var paymentResult = generatedResult {req.query.resourcePath function ( result ) {
+	if ( result != null && result !=undefined )
+		res.render('result', {message: result.result.description});
+	else 
+		res.render('result', 'none')
+	};
+};
+
+
+function generatedResultpath, callback) {
+	return callback(null);
+}
